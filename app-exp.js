8d926ed9d33dfff935b735e7cf2fcc8f9c04fce0
Sujan <786sksujanislam786@gmail.com>
2021-01-16 23:20:46 +0530

    Added gitignore and  Procfile

--- app-exp.js
+++ app-exp.js
@@ -152,8 +152,12 @@ app.post("/delete", function(req,res){
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function (){
+app.listen( port, function (){
     console.log("Take off to 3000")
 })
 
